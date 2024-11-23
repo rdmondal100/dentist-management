8aa200f19986cd02b00c70b5c334ebd1b5bb6da8
Riday <rdmondal100@gmail.com>
2024-11-23 09:17:19 +0600

    first commit

---
+++ backend/config/connectDb.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+
+const connectDb = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(connectionInstance)
+    } catch (error) {
+        console.log("Error connecting database::",error)
+        process.exit(1)
+    }
+}
+
+
+export default connectDb
---
+++ backend/server.js
@@ -0,0 +1,7 @@
+import express from 'express'
+import 'dotenv/config'
+import connectDb from './config/connectDb'
+
+
+
+const app = express()
