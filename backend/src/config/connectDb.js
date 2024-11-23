73d4e24b89a4668472f2f1fd33282ea9044cc067
Riday <rdmondal100@gmail.com>
2024-11-23 09:32:02 +0600

    setup the project staructure

--- backend/src/config/connectDb.js
+++ backend/src/config/connectDb.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 
-const connectDb = async()=>{
+
+const connectDb = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(connectionInstance)
     } catch (error) {
-        console.log("Error connecting database::",error)
+        console.log("Error connecting database::", error)
         process.exit(1)
     }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import 'dotenv/config'
-import connectDb from './config/connectDb'
+import connectDb from './config/connectDb.js'
 
 
 
